0338eea7e7962565aa335e564c4e7fd0a3092148
Sydney Grant <sg3957@nyu.edu>
2018-09-30 03:40:16 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,11 @@ def first_challenge
   }
 
   #your code here
-
+  contacts.each do |person, data|
+    data.each do |attribute, value|
+      if attribute == :favorite_icecream_flavors
+       value.each do |flavor|
+         value.delete_if("strawberry")
 
   #remember to return your newly altered contacts hash!
   contacts

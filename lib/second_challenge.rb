487e4564096cf9e32c587fe31c98dd6464647e30
Sydney Grant <sg3957@nyu.edu>
2018-09-30 04:15:25 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -7,11 +7,6 @@ def second_challenge
   }
 
   #code your solution here!
-  # flat_arr = []
-  # flat_arr << groceries.values
-  # flat_arr << groceries[vegetable].values
-  # flat_arr << groceries[meat].values
-  # flat_arr << groceries[grains].values
-  # flat_arr
+ 
 groceries.values.flatten
 end
